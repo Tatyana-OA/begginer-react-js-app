6d48cdb993ed3a27c2d2ca399d54e1ea33fc1adf
Tatyana Asenova <tatyana.asenova@augeocorp.net>
2022-11-15 12:25:25 +0200

    FlatList exercise

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,23 +3,21 @@ import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friend 1" },
-    { name: "Friend 2" },
-    { name: "Friend 3" },
-    { name: "Friend 4" },
-    { name: "Friend 5" },
-    { name: "Friend 6" },
+    { name: "Friend 1", age: 25 },
+    { name: "Friend 2", age: 27 },
+    { name: "Friend 3", age: 35 },
+    { name: "Friend 4", age: 40 },
+    { name: "Friend 5", age: 20 },
+    { name: "Friend 6", age: 18 },
   ];
   return (
     <FlatList
       keyExtractor={(friend) => friend.name} // use Name property as key
-      horizontal // makes flatlist horizontal
-      showsHorizontalScrollIndicator={false} // overflow hidden
       data={friends}
       renderItem={({ item, index }) => {
         return (
           <Text key={index} styles={styles.textStyle}>
-            {item.name}
+            {item.name} - Age {item.age}
           </Text>
         );
       }}
