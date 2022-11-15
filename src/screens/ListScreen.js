d9211c151c039d7a466bf766bddd8caab97ccf62
Tatyana Asenova <tatyana.asenova@augeocorp.net>
2022-11-15 12:02:02 +0200

    FlatList example

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+
+const ListScreen = () => {
+  const friends = [
+    { name: "Friend 1" },
+    { name: "Friend 2" },
+    { name: "Friend 3" },
+    { name: "Friend 4" },
+    { name: "Friend 5" },
+    { name: "Friend 6" },
+  ];
+  return (
+    <FlatList
+      data={friends}
+      renderItem={({ item }) => {
+        return <Text>{item.name}</Text>;
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create({});
+export default ListScreen;
