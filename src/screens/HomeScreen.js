202f1289745402d2e54c915021cbc35b015e674d
Tatyana Asenova <tatyana.asenova@augeocorp.net>
2022-11-15 13:10:26 +0200

    Navigation using stackNavigator and props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hello, Darkness, my old friend.</Text>
       <Button
         title="Go to Components Demo"
         onPress={() => {
-          console.log("Click");
+          navigation.navigate("Components");
         }}
       />
-      <TouchableOpacity onPress={() => console.log("Press touchable opacity")}>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
     </View>
   );
 };
